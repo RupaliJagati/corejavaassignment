9ab8dbb591fddbfcfd9dddbc6b67325325298453
RupaliJagati <63409986+RupaliJagati@users.noreply.github.com>
2020-09-24 14:25:07 +0530

    Add files via upload

--- Q46.java
+++ Q46.java
@@ -1,60 +1,73 @@
-//46
-import java.util.*;
+import java.lang.*;
 
-class shape{
-	int length;
-	int height;
-	shape(){
-		
-	}
-	shape(int length,int height){
-		this.length=length;
-		this.height=height;
-	}
-	void display(){
-		System.out.println("length="+length+"height="+height);
-	}
-	void area(){}
-	
-}
-class rectangle extends shape{
-	rectangle(){}
-	rectangle(int length,int height)
-	{
-		super(length,height);
-	}
-	void area(){
-		super.display();
-		System.out.println("area :"+(length*height));
-	}
-	void diagonal(){
-		System.out.println("diagonal :"+(Math.sqrt(length^2 + height^2)));
-	}
-	
-	
-}
-class triangle extends shape{
-	triangle(){}
-	triangle(int length,int height)
-	{
-		super(length,height);
-	}
-	void area(){
-		super.display();
-		System.out.println("area :"+(0.5 * length * height));
-	}
+class Shape {
+
+    int length;
+    int height;
+
+    void show() {
+        System.out.println("Length: " + length + " height: " + height);
+    }
+
+    Shape() {
+    }
+
+    Shape(int length, int height) {
+        this.length = length;
+        this.height = height;
+    }
+
+    void Area() {
+    };
+
 }
 
-public class Q46{
+class Rectangle extends Shape {
 
-	public static void main(String[] args) {
-		 shape sarr[]={new rectangle(30,40),new triangle(10,20)};
-		 sarr[0].area();
-		 sarr[1].area();
-		 rectangle r=(rectangle)sarr[0];
-		 r.diagonal();
-		 
-	    
-	    
-	}
+    void Area() {
+        super.show();
+        System.out.println("Area:  " + (length * height));
+    }
+
+    void diagonal() {
+        double a = Math.pow(length, 2) + Math.pow(height, 2);
+        System.out.println(" Diagonal is: " + (Math.sqrt(a)));
+    }
+
+    Rectangle() {
+    }
+
+    Rectangle(int length, int height) {
+        super(length, height);
+    }
+
+}
+
+class Triangle extends Shape {
+    void Area() {
+        super.show();
+        System.out.println("Area:  " + (0.5 * length * height));
+    }
+
+    Triangle() {
+    }
+
+    Triangle(int length, int height) {
+        super(length, height);
+    }
+}
+
+public class Q46 {
+    public static void main(String[] args) {
+
+        Shape sArr[] = { new Rectangle(30, 40), new Triangle(10, 20) };
+
+        sArr[0].Area();
+
+        Rectangle r = (Rectangle) sArr[0];
+        r.diagonal();
+        sArr[1].Area();
+
+    }
+
 }
